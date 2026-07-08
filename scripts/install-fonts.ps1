8d2c236abb3c45d9eccd88cea192321f6738f42a
Morten Stig <stig9999@hotmail.com>
2026-07-08 15:23:27 +0200

    Refactor font installation script for clarity

--- scripts/install-fonts.ps1
+++ scripts/install-fonts.ps1
@@ -1,7 +1,14 @@
 $ErrorActionPreference = "Stop"
 
+Write-Step "Installing fonts"
+
 if (-not (Get-Command oh-my-posh -ErrorAction SilentlyContinue)) {
-    throw "oh-my-posh must be installed before installing fonts."
+    throw "Oh My Posh must be installed before installing fonts."
 }
 
-oh-my-posh font install CaskaydiaCove
+$font = "CaskaydiaCove"
+
+Write-Info "Installing $font Nerd Font..."
+oh-my-posh font install $font
+
+Write-Success "$font Nerd Font installed."
