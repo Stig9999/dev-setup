8ef36558c0667b9445a2f9e58bb75c7115ee0cb1
Morten Stig <stig9999@hotmail.com>
2026-07-08 15:23:03 +0200

    Remove kubectl from installation script

--- scripts/install-apps.ps1
+++ scripts/install-apps.ps1
@@ -1,12 +1,14 @@
+$ErrorActionPreference = "Stop"
+
 Write-Step "Installing applications"
 
 $packages = @(
+    "Git.Git",
     "Microsoft.PowerShell",
     "Microsoft.WindowsTerminal",
     "JanDeDobbeleer.OhMyPosh",
     "JetBrains.Toolbox",
     "Microsoft.VisualStudioCode",
-    "Kubernetes.kubectl",
     "Docker.DockerDesktop"
 )
 
