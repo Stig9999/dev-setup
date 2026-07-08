587ea5c42e2c890bd24de5021e33cd83740a3416
Morten Stig <stig9999@hotmail.com>
2026-07-08 15:24:03 +0200

    Refactor Windows Terminal setup script for clarity

--- scripts/setup-windows-terminal.ps1
+++ scripts/setup-windows-terminal.ps1
@@ -14,8 +14,10 @@ if (-not $terminalPackage) {
     throw "Windows Terminal installation could not be found."
 }
 
+$destination = Join-Path $terminalPackage.FullName "LocalState\settings.json"
+
 Copy-File `
     -Source $source `
-    -Destination (Join-Path $terminalPackage.FullName "LocalState\settings.json")
+    -Destination $destination
 
 Write-Success "Windows Terminal configured."
