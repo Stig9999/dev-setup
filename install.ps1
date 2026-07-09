a504e025ee1d15293cfe3a751465ef283f466a19
Morten Stig <stig9999@hotmail.com>
2026-07-09 09:10:57 +0200

    Refactor install.ps1 for improved readability

--- install.ps1
+++ install.ps1
@@ -6,7 +6,6 @@ Write-Step "Installing development setup"
 
 & "$PSScriptRoot\scripts\install-apps.ps1"
 & "$PSScriptRoot\scripts\install-fonts.ps1"
-& "$PSScriptRoot\scripts\setup-git.ps1"
 & "$PSScriptRoot\scripts\setup-powershell.ps1"
 & "$PSScriptRoot\scripts\setup-windows-terminal.ps1"
 
